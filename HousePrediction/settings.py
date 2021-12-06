7d7bbf519e6d93da69a80fbbc18fbf23d57709e4
qbdq <58635995+qbdq@users.noreply.github.com>
2021-12-06 23:43:14 +0800

    second try to deploy to heroku

--- HousePrediction/settings.py
+++ HousePrediction/settings.py
@@ -31,6 +31,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Application definition
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
